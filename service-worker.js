438698260c19831f6aa2920cbb8c357c1fe447e4
1982jwar <marioluiz1982@gmail.com>
2026-04-28 07:27:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const CACHE_NAME = 'karaoke-cache-v5'; // 
+=======
+const CACHE_NAME = 'karaoke-cache-v2'; // 
+>>>>>>> 96cd7e0ce8713cb10b11cff8166b0ff73d95108b
 const ASSETS = [
   './',
   './index.html',
