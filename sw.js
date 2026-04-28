8e877f85c03688e6dafd709ebb2f51dfaf439cbc
1982jwar <bombapatchcampos@gmail.com>
2026-04-28 08:33:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'karaoke-cache-v5';
 const CACHE_NAME = 'karaoke-cache-v2';
 >>>>>>> 96cd7e0ce8713cb10b11cff8166b0ff73d95108b
 =======
-const CACHE_NAME = 'karaoke-cache-v2';
+const CACHE_NAME = 'karaoke-cache-v10';
 >>>>>>> Stashed changes
 
 // Arquivos para cache (atualize com seus caminhos)
